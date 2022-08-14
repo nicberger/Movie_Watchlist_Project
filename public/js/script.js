a69be459238fbc5156730b485fc249cfa7548ca2
Nico Schmock <nicoschmock@web.de>
2022-08-14 17:14:23 +0200

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("Project_Ironhack_Daiane_Nico JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Project_Ironhack_Daiane_Nico";
+
+module.exports = MONGO_URI;
