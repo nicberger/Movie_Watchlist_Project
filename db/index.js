2e2b5f3c72e76c768aa6068f0557d0538682850c
Daiane Reina <daiane.veras.reina@gmail.com>
2022-08-18 23:40:40 +0100

    testing the conection with the database amd heroko

--- db/index.js
+++ db/index.js
@@ -5,15 +5,23 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = require("../utils/consts");
+// const MONGO_URI = require("../utils/consts");
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+const MONGO_URL = require("../utils/consts");
+
+mongoose.connect(MONGO_URL).then((connection) => {
+  console.log(
+    `Connected to Mongo!! Database name is "${connection.connections[0].name}"`
+  );
+});
+
+// mongoose
+//   .connect(MONGO_URI)
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo: ", err);
+//   });
