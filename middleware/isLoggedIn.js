7136a6d98ecf897f07d9f01fe6cc6ac8c2fb9b44
Daiane Reina <daiane.veras.reina@gmail.com>
2022-08-18 19:44:18 +0100

    handlebars!

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,10 @@
-module.exports = (req, res, next) => {
+function isLoggedIn(req, res, next) {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.user) {
     return res.redirect("/auth/login");
   }
   req.user = req.session.user;
   next();
-};
+}
+
+module.exports = isLoggedIn;
