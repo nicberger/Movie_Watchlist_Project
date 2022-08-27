726ed0e7ceb70cb8ada88a09605bd477fb4c06cb
Nico Schmock <nicoschmock@web.de>
2022-08-27 14:29:07 +0200

    update before merge2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  const isLoggedIn = req.session.user ? true : false;
-  // console.log("this is logged in", isLoggedIn)
-  res.render("index", { isLoggedIn });
+    const isLoggedIn = req.session.user ? true : false;
+    // console.log("this is logged in", isLoggedIn)
+    res.render("index", { isLoggedIn });
 });
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,21 +5,21 @@ const UserModel = require("../models/User.model");
 const userRoutes = Router();
 
 userRoutes.get("/:userId", (req, res) => {
-  UserModel.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        return res.redirect("/");
-      }
+    UserModel.findById(req.params.userId)
+        .then((user) => {
+            if (!user) {
+                return res.redirect("/");
+            }
 
-      res.render("user/personal", {
-        user: user,
-        userId: req.params.userId,
-      });
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.status(500).redirect("/");
-    });
+            res.render("user/personal", {
+                user: user,
+                userId: req.params.userId,
+            });
+        })
+        .catch((err) => {
+            console.log("err", err);
+            res.status(500).redirect("/");
+        });
 });
 
 module.exports = userRoutes;
