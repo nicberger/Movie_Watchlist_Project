055f88b079781fb5f4bb40d5a2022b1fbbade083
Nico Schmock <nicoschmock@web.de>
2022-08-27 16:42:23 +0200

    atlas db

--- db/index.js
+++ db/index.js
@@ -5,9 +5,12 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = require("../utils/consts");
+const MONGO_URL = require("../utils/consts");
+// const MONGO_URL =
+//     "mongodb+srv://nicberger:pallmall356@cluster0.4smeofm.mongodb.net/?retryWrites=true&w=majority" ||
+//     "mongodb://localhost:27017";
 
-mongoose.connect(MONGO_URI).then((connection) => {
+mongoose.connect(MONGO_URL).then((connection) => {
     console.log("Connected to Mongo" + MONGO_URL);
 });
 
